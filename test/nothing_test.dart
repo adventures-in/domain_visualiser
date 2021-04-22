cd6b1a927428ae2f8ed93d2e3c0188267e647f59
Nick Meinhold <nick.meinhold@gmail.com>
2021-04-22 13:39:49 +1000

    Add step to CI to copy index.auto to index.html (#4)
    * Add step to CI to copy index.auto to index.html
    
    * Fix errant script tag
    
    * Add build script to CI
    
    * Add Flutter setup to CI
    
    * Remove all commited generated files
    
    * Give flutter test something to play with
    
    * Point public directory to build/web

---
+++ test/nothing_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('InitialPage', () {
+    testWidgets('InitialPage ...', (WidgetTester tester) async {
+      expect(true, true);
+    });
+  });
+}
