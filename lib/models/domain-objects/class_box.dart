8b7cdf02c581f678e712dc0e77b7c71682a3d349
Nick Meinhold <nick.meinhold@gmail.com>
2021-04-22 23:33:33 +1000

    Add userId to ClassBox (#6)

--- lib/models/domain-objects/class_box.dart
+++ lib/models/domain-objects/class_box.dart
@@ -6,17 +6,26 @@ part 'class_box.g.dart';
 
 @freezed
 class ClassBox with _$ClassBox {
-  factory ClassBox(
-      {required String id,
-      required double left,
-      required double top,
-      required double right,
-      required double bottom,
-      String? name,
-      IList<String>? staticMethods,
-      IList<String>? instanceMethods,
-      IList<String>? staticVariables,
-      IList<String>? instanceVariables}) = _ClassBox;
+  factory ClassBox({
+    /// The id of the ClassBox
+    required String id,
+
+    /// The id of the user that created the ClassBox
+    String? userId,
+
+    /// X,Y positions of the sides
+    required double left,
+    required double top,
+    required double right,
+    required double bottom,
+
+    /// Metadata for the ClassBox
+    String? name,
+    IList<String>? staticMethods,
+    IList<String>? instanceMethods,
+    IList<String>? staticVariables,
+    IList<String>? instanceVariables,
+  }) = _ClassBox;
 
   factory ClassBox.fromJson(Map<String, dynamic> json) =>
       _$ClassBoxFromJson(json);
